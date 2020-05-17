03e0e2d4ea716cd18c5592526d14c4dc59b186da
Christina Sohn <chsohn15@gmail.com>
2020-05-17 04:34:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,7 @@
 # Write your code here
 count = 10
-while count <= 10 && count > 0 do 
+while count <= 10 && count >= 1 do 
   puts count
   count -= 1
-  if count == 1  
-    puts "Happpy New Year!"
   end
-  
+  puts "Happpy New Year!"
